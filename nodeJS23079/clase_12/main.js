3b21bc145536680e9f80b6aa6f17037040783be3
Julio Mata <julioematasv@gmail.com>
2023-11-10 09:38:34 +0100

    Iniciando clase 12 Javascript, ejemplo de uso de console y como insertar texto html

---
+++ nodeJS23079/clase_12/main.js
@@ -0,0 +1,7 @@
+document.getElementById('titulo').style.color = 'blue';
+
+let entrada = prompt('Ingresa tu nombre ');
+
+document.getElementById('nombre').innerHTML = entrada;
+
+console.log(entrada);
