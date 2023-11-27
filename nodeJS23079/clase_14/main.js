65dd8ef26e75d86f444e4894c189ea5b897c1d1b
Julio Mata <julioematasv@gmail.com>
2023-11-27 09:36:33 +0100

    Realizados ejercicios de ciclos while

---
+++ nodeJS23079/clase_14/main.js
@@ -0,0 +1,34 @@
+/* let condition = Number(prompt('1. Ver Saldo\n 0. Salir', " "));
+
+while (condition != 0){
+    switch (condition){
+        case 1:
+            document.getElementById('siCumple').innerHTML = 'Su saldo es de € 100.00';
+            condition = 0;
+            break;
+        default:
+            document.getElementById('incorrecta').innerHTML = 'La opción es incorrecta vueva a elegir.';
+    }
+    condition = Number(prompt('1. Ver Saldo\n 0. Salir'));
+} */
+
+/* while (condition != 0) {
+    switch (condition){
+        case 1:
+            alert('Su saldo es de € 100.00');
+            condition = 0;
+            break;
+        default:
+            alert('La opción es incorrecta vueva a elegir.');
+            break;
+    }
+    condition = Number(prompt('1. Ver Saldo\n 0. Salir'));
+} */
+
+/* let startNumber = Number(prompt('Un número para el inicio'));
+let endNumber = Number(prompt('Un número para el Final'));
+
+while (startNumber <= endNumber) {
+    document.write("El valor del bucle es: " + startNumber + "<br>");
+    startNumber++;
+} */
